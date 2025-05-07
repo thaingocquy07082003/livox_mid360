e863e0486a6e7dcabc71adac908fede6a8ff5b2c
thai quy <thaingocquy2003@gmail.com>
2025-05-07 23:05:46 +0700

    run main.cpp for test connection

---
+++ mid360_test_connection.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <chrono>
+// #include <livox_lidar_api.h>
+#include<livox_lidar_api.h>
+#include<livox_lidar_def.h>
+int main() {
+    
+    return 0;
+}
